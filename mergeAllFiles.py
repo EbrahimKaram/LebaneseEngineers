e0e6c6db5e991a36f7414da0cd6e1255c1656f1e
Ebrahim Karam <ebrahim_karam@hotmail.com>
2021-02-06 19:13:51 -0500

    Merged all the Engineers into one csv
    This would everyone and all the engineers.
    This would be wonderful for data analysis and machine learning

---
+++ mergeAllFiles.py
@@ -0,0 +1,16 @@
+import glob
+import pandas as pd
+
+
+if __name__ == '__main__':
+    files = glob.glob("Data/*[0-9].csv")
+    print(files)
+    print(len(files))
+    dfs = []
+    for file in files:
+        df = pd.read_csv(file, encoding="utf-8")
+        dfs.append(df)
+
+    all_engineers = pd.concat(dfs, ignore_index=True)
+
+    all_engineers.to_csv("Data/all_engineers.csv", index=False)
